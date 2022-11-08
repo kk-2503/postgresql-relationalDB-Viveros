4acc8f32adda66d939f0f8504b46e9ea7ede4bd7
kk-2503 <alu0101109046@ull.edu.es>
2022-11-08 19:32:54 +0000

    Añadido tabla vivero en script

--- script.sql
+++ script.sql
@@ -1,28 +1,48 @@
 DROP TABLE IF EXISTS empleado;
 DROP TABLE IF EXISTS zona;
+DROP TABLE IF EXISTS vivero;
+
+CREATE TABLE vivero (
+	vivero_id INT PRIMARY KEY,
+	nombre VARCHAR(100) NOT NULL,
+	direccion VARCHAR(300) NOT NULL
+);
+
+INSERT INTO vivero
+VALUES(11, 'VIVERO1', 'DIRECCION1');
+
+SELECT *
+FROM vivero;
 
 CREATE TABLE zona (
 	zona_id INT PRIMARY KEY,
-	nombre VARCHAR(50)
+	nombre VARCHAR(50) NOT NULL,
+	vivero_id INT NOT NULL,
+	CONSTRAINT fk_vivero
+	FOREIGN KEY(vivero_id)
+	REFERENCES vivero(vivero_id)
 );
 
 INSERT INTO zona
-VALUES(1, 'ALMACEN');
+VALUES(1, 'ALMACEN', 11);
 
 SELECT *
 FROM zona;
 
 CREATE TABLE empleado (
 	dni VARCHAR(9) PRIMARY KEY,
-	nombre VARCHAR(50),
-	zona_id INT,
+	nombre VARCHAR(50) NOT NULL,
+	puesto VARCHAR(150) NOT NULL,
+	inicio DATE NOT NULL CHECK (inicio > '1900-01-01'),
+	fin DATE,
+	zona_id INT NOT NULL,
 	CONSTRAINT fk_zona
 	FOREIGN KEY(zona_id)
 	REFERENCES zona(zona_id)
 );
 
 INSERT INTO empleado
-VALUES('12345678Z', 'PEPE', 1);
+VALUES('12345678Z', 'PEPE', 'ABC', '2022-11-01', NULL,1);
 
 SELECT *
 FROM empleado;
